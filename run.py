9e7e9faba893781da739bf0bb0b5bd593b488ccf
chan <90619921+chani017@users.noreply.github.com>
2021-11-14 01:31:51 +0900

    Add files via upload

---
+++ run.py
@@ -0,0 +1,12 @@
+# run.py
+
+from Class_func1 import Class_func1
+
+# 인스턴스 생성
+inst_1 = Class_func1()
+
+# 메소드 호출
+inst_1.function_1(10, 20)
+
+print(inst_1.num1, inst_1.num2)
+print(inst_1.function_2())

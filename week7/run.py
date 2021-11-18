8fc55ddb23f1c13bb350589dcc5e10702c85ddc2
chan <90619921+chani017@users.noreply.github.com>
2021-11-18 20:37:54 +0900

    Update run.py

--- week7/run.py
+++ week7/run.py
@@ -1,5 +1,8 @@
 from Date import Date
 
-inst_1 = Date()
+year = int(input("년도를 입력하세요 : "))
+month = int(input("월을 입력하세요 : "))
+date = int(input("일을 입력하세요 : "))
 
-print(inst_1.is_valid_date())
+inst_1 = Date(year, month, date)
+inst_1.is_valid_date()

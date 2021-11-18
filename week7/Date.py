fac6abd469675e0ab78839cddbab5c01e04248f6
chan <90619921+chani017@users.noreply.github.com>
2021-11-18 19:53:30 +0900

    Add files via upload

---
+++ week7/Date.py
@@ -0,0 +1,31 @@
+# Date.py
+# import 금지
+
+class Date:
+    def __init__(self, year, month, date):
+        self.year = year
+        self.month = month
+        self.date = date
+
+    def is_valid_date(self, year, month, date):
+        if  year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
+            if (month % 2) == 1: #31일이 있는 달
+                if month <= 7:
+                    if date <= 0 or date > 31:
+                        return False
+                else: #month > 7
+                    if date <= 0 or date > 30:
+                        return False
+        else: #31일이 없는 달
+            if month <= 6:
+                if month == 2:
+                    if date <= 0 or date > 29:
+                        return False
+                    elif date <= 0 or date > 28:
+                        return False
+                if date <= 0 or date > 30:
+                    return False
+            else: 
+                if date <= 0 or date > 31:
+                    return False
+        return True
---
+++ week7/run.py
@@ -0,0 +1,7 @@
+from Date import Date
+
+inst_1 = Date()
+
+inst_1.is_valid_date(2011, 11, 30)
+
+print(inst_1.is_valid_date)

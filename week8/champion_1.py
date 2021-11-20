f5ca3876165c90f388121ef5df34dac6ca357b1a
chan <90619921+chani017@users.noreply.github.com>
2021-11-20 12:28:09 +0900

    Update champion_1.py

--- week8/champion_1.py
+++ week8/champion_1.py
@@ -1,25 +1,22 @@
 class Champion:
     def __init__(self, name):
         self.name = name
+        self.hp = 1000
+        self.cure = 15
+        self.speed = 300
 
 class Mana_Champion(Champion):
     def __init__(self, name):
-        self.name = name
-        self.hp = 1000
-        self.cure = 15
+        super().__init__(name)
         self.mana = 1000
-        self.speed = 300
 
     def cham_info(self):
-        print(f"-----------------현재{self.name}정보-----------------")
+        print(f"-----------------현재{self.name}정보-----------------")Z
         print(f"체력 : {self.hp}, 회복 : {self.cure}, 마나 : {self.mana}, 속도 : {self.speed}")
 
 class Mana_none_Champion(Champion):
     def __init__(self, name):
-        self.name = name
-        self.hp = 1000
-        self.cure = 15
-        self.speed = 300
+        super().__init__(name)
         
     def cham_info(self):
         print(f"-----------------현재{self.name}정보-----------------")
